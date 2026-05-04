677fffccff0d17d2b238e75f9cf988afb94ebd55
Matthew Abruzzo <matthewabruzzo@gmail.com>
2026-05-03 20:22:31 -0400

    move the declaration and definition of tau_con to right before it's actually used

--- src/clib/cool1d_multi_g.cpp
+++ src/clib/cool1d_multi_g.cpp
@@ -186,7 +186,6 @@ void cool1d_multi_g(
   std::vector<double> alpha_continuum(my_fields->grid_dimension[0]);
   std::vector<double> alphad(my_fields->grid_dimension[0]);
   std::vector<double> lshield_con(my_fields->grid_dimension[0]);
-  std::vector<double> tau_con(my_fields->grid_dimension[0]);
   double log_a;
 
   const gr_opaque_storage& opaque_storage = *my_rates->opaque_storage;
@@ -912,12 +911,6 @@ void cool1d_multi_g(
     }
   }
 
-  for (i = idx_range.i_start; i <= idx_range.i_end; i++) {
-    if (itmask[i] != MASK_FALSE) {
-      tau_con[i] = alpha_continuum[i] * lshield_con[i];
-    }
-  }
-
   // --- Compute (external) radiative heating terms ---
   // Photoionization heating
 
@@ -1337,6 +1330,14 @@ void cool1d_multi_g(
   }
 
   // Continuum opacity
+
+  // todo: stop allocating a buffer for tau_con
+  std::vector<double> tau_con(my_fields->grid_dimension[0]);
+  for (i = idx_range.i_start; i <= idx_range.i_end; i++) {
+    if (itmask[i] != MASK_FALSE) {
+      tau_con[i] = alpha_continuum[i] * lshield_con[i];
+    }
+  }
 
   for (i = idx_range.i_start; i <= idx_range.i_end; i++) {
     if (itmask[i] != MASK_FALSE) {
