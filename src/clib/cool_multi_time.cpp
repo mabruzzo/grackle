e42684b9f9a05d8b06469546b83d1541fdd6cf93
Matthew Abruzzo <matthewabruzzo@gmail.com>
2026-08-19 00:36:24 -0400

    hoist call to DustSolver::calc_Tdust_and_chem_contrib out of cool1d_multi_g

--- src/clib/cool_multi_time.cpp
+++ src/clib/cool_multi_time.cpp
@@ -137,16 +137,23 @@ void cool_multi_time(
         alpha_continuum[i] = 0.0;  // <- this is redundant (its already 0)
       }
 
+      // based on precise configuration, perform a subset of:
+      // - compute Tdust, dust2gas
+      // - add contributions to alpha_continuum, edot from dust
+      my_rates->opaque_storage->dust_solver.calc_Tdust_and_chem_contrib(
+          edot.data(), dust2gas.data(), tdust.data(), grain_temperatures,
+          alpha_continuum.data(), tgas.data(), rhoH.data(),
+          nelec_times_mH.data(), metallicity.data(), itmask.data(),
+          itmask_metal.data(), my_chemistry, my_rates, my_fields,
+          species_densities, internalu, idx_range, logTlininterp_buf);
+
       // compute edot
       cool1d_multi_g(
-        edot.data(), alpha_continuum.data(), tgas.data(),
-        mmw.data(), tdust.data(), metallicity.data(),
-        dust2gas.data(), rhoH.data(), nelec_times_mH.data(), 
-        itmask.data(), itmask_metal.data(),
-        my_chemistry, my_rates, my_fields, species_densities,
-        my_uvb_rates, internalu, idx_range,
-        grain_temperatures, logTlininterp_buf, coolingheating_buf
-      );
+          edot.data(), alpha_continuum.data(), tgas.data(), mmw.data(),
+          metallicity.data(), rhoH.data(), nelec_times_mH.data(), itmask.data(),
+          itmask_metal.data(), my_chemistry, my_rates, my_fields,
+          species_densities, my_uvb_rates, internalu, idx_range,
+          logTlininterp_buf, coolingheating_buf);
 
       // Compute the cooling time on the slice
       //   (the gamma used here is the same as used to calculate the pressure
