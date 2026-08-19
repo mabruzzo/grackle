bea53fc27fa09fe8e39a0e1a09d39e35961736e4
Matthew Abruzzo <matthewabruzzo@gmail.com>
2026-08-18 22:09:34 -0400

    eliminate all references to Cool1DMultiScratchBuf

--- src/clib/step_rate_newton_raphson.hpp
+++ src/clib/step_rate_newton_raphson.hpp
@@ -150,7 +150,7 @@ inline void wrapped_calc_derivatives(
 /// @note
 /// The values in the various buffers (tgas, tdust, metallicity, dust2gas,
 /// rhoH, mmw, edot, grain_temperatures, logTlininterp_buf,
-/// cool1dmulti_buf, coolingheating_buf, chemheatrates_buf), hold undefined
+/// coolingheating_buf, chemheatrates_buf), hold undefined
 /// values after this function call wherever `itmask_nr` indicates that this
 /// function runs.
 /// - this has **ALWAYS** been the case. Historically these buffers have been
@@ -166,7 +166,6 @@ inline void step_rate_newton_raphson(
     photo_rate_storage my_uvb_rates, InternalGrUnits internalu,
     grackle::impl::GrainSpeciesCollection grain_temperatures,
     grackle::impl::LnTLinInterpBuf logTlininterp_buf,
-    grackle::impl::Cool1DMultiScratchBuf cool1dmulti_buf,
     grackle::impl::CoolHeatScratchBuf coolingheating_buf,
     grackle::impl::ChemHeatingRates chemheatrates_buf) {
   // shorten `grackle::impl::time_deriv_0d` to `t_deriv` within this function
@@ -505,12 +504,10 @@ inline void step_rate_newton_raphson(
       // -> in a lot of cases, the need to copy values is dictated by the value
       //    of imp_eng
       // -> in the future, we should really refactor so that this isn't
-      //    necessary (arguably, the only thing we should really consider
-      //    copying is the value of cool1dmulti_buf.tgasold -- and that doesn't
-      //    currently get used)
+      //    necessary
       t_deriv::scratchbufs_copy_into_pack(
         i, &pack, tgas, tdust, metallicity, dust2gas, rhoH, mmw, nelec_times_mH,
-        edot, grain_temperatures, logTlininterp_buf, cool1dmulti_buf,
+        edot, grain_temperatures, logTlininterp_buf,
         coolingheating_buf, chemheatrates_buf
       );
 
@@ -660,7 +657,7 @@ inline void step_rate_newton_raphson(
       //    do so)
       t_deriv::scratchbufs_copy_from_pack(
         i, &pack, tgas, tdust, metallicity, dust2gas, rhoH, mmw, nelec_times_mH,
-        edot, grain_temperatures, logTlininterp_buf, cool1dmulti_buf,
+        edot, grain_temperatures, logTlininterp_buf,
         coolingheating_buf, chemheatrates_buf
       );
 
